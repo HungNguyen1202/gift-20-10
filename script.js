b684547a91e1017bcb3f4a7cb49e4edb15fff67e
Hung Nguyen <hng.ngn1202@gmail.com>
2024-10-13 14:30:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("confetti")[0].addEventListener("click", () => {
+    confetti({
+        particleCount: 100,
+        startVelocity: 30,
+        spread: 360,
+        origin: {
+          x: Math.random(),
+          // since they fall down, start a bit higher than random
+          y: Math.random() - 0.2
+        }
+      });
+});
